04c88ff62d07e9af457c4320a446be1f773907c1
Julien Amiel <129734658+Corvaxx117@users.noreply.github.com>
2023-12-18 16:20:10 +0100

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,11 +1,8 @@
 <?php
 
-// define("DB_HOST", 'db.3wa.io');
-// define("DB_NAME", 'julienamiel_aveemana');
-// define("DB_USER", 'julienamiel');
-// define("DB_PASS", 'fb9667e6f2500024445ca3ddf6a2777d');
+// define("DB_HOST", '******');
+// define("DB_NAME", '******');
+// define("DB_USER", '******');
+// define("DB_PASS", '******');
 
-define("DB_HOST", 'localhost:8889');
-define("DB_NAME", 'aveemana');
-define("DB_USER", 'root');
-define("DB_PASS", '040991');
+
